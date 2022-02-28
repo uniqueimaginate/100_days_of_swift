7bee8c447d146c95a02cfce181f7e5a279fe38ac
Donguk Lee <uniqueimaginate@gmail.com>
2022-02-28 15:03:15 +0900

    Day34

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -10,19 +10,46 @@ import UIKit
 class ViewController: UITableViewController {
 
     var petitions = [Petition]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 // we're OK to parse!
                 parse(json: data)
+            } else {
+                showError()
             }
+        } else {
+            showError()
         }
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(alert))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filterPetitions))
+    }
+    
+    @objc func alert(){
+        let ac = UIAlertController(title: "Credit", message: "Data is coming from \'We The People API of the Whitehouse\'", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        ac.addAction(action)
+        present(ac, animated: true, completion: nil)
+    }
+    
+    @objc func filterPetitions(){
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,6 +64,12 @@ class ViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     func parse(json: Data) {
         let decoder = JSONDecoder()
 
@@ -45,7 +78,12 @@ class ViewController: UITableViewController {
             tableView.reloadData()
         }
     }
-
-
+    
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
 }
 
