773c7c641b5972a0bdc8d8b8930e032a2facd09d
Donguk Lee <uniqueimaginate@gmail.com>
2022-03-10 23:34:54 +0900

    Day48

---
+++ Project12b/Project12b/Person.swift
@@ -0,0 +1,30 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Peter on 2022/03/06.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    
+}
