faeb8f3575d62ec77d438d195f69e893c2dd4235
Donguk Lee <uniqueimaginate@gmail.com>
2022-03-13 12:08:49 +0900

    Day52

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Peter on 2022/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+    
+    @objc func importPicture(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+        
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+
+        dismiss(animated: true)
+        imageView.image = image
+        currentImage = image
+    }
+}
+
