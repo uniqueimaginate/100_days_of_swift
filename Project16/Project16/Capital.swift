b59fc5d1b277a672c3b92365f02610e3d311a2e8
Donguk Lee <uniqueimaginate@gmail.com>
2022-03-13 23:40:58 +0900

    Day60

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Peter on 2022/03/13.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String){
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
