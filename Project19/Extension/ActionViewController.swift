c1a9201460a5796610b42cd53e773bcbf9fbc287
Donguk Lee <uniqueimaginate@gmail.com>
2022-03-17 22:15:35 +0900

    Day69

--- Project19/Extension/ActionViewController.swift
+++ Project19/Extension/ActionViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 import MobileCoreServices
 import UniformTypeIdentifiers
 
-class ActionViewController: UIViewController {
+class ActionViewController: UIViewController{
 
-    @IBOutlet var script: UITextView!
+    var scripts = [Script]()
     
+    @IBOutlet var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
     var pageTitle = ""
     var pageURL = ""
     
@@ -39,38 +45,84 @@ class ActionViewController: UIViewController {
             }
         }
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
         
     }
     
     @objc func adjustForKeyboard(notification: Notification) {
-        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-
-        let keyboardScreenEndFrame = keyboardValue.cgRectValue
-        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
-
-        if notification.name == UIResponder.keyboardWillHideNotification {
-            script.contentInset = .zero
-        } else {
-            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
-        }
-
-        script.scrollIndicatorInsets = script.contentInset
-
-        let selectedRange = script.selectedRange
-        script.scrollRangeToVisible(selectedRange)
+//        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+//
+//        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+//        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+//
+//        if notification.name == UIResponder.keyboardWillHideNotification {
+//            script.contentInset = .zero
+//        } else {
+//            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+//        }
+//
+//        script.scrollIndicatorInsets = script.contentInset
+//
+//        let selectedRange = script.selectedRange
+//        script.scrollRangeToVisible(selectedRange)
     }
 
-    @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
+    @IBAction func add() {
+        
+        let ac = UIAlertController(title: "Add Your Own Script", message: "Script", preferredStyle: .alert)
+        
+        ac.addTextField()
+        ac.addTextField()
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let addAction = UIAlertAction(title: "Add", style: .default) { [weak ac, weak self] _ in
+            let title = ac?.textFields![0].text
+            let script = ac?.textFields![1].text
+            
+            self?.scripts.append(Script(title: title!, content: script!))
+            self?.tableView.reloadData()
+        }
+        
+        
+        ac.addAction(cancelAction)
+        ac.addAction(addAction)
+        
+        present(ac, animated: true)
+
+    }
+
+}
+
+
+extension ActionViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return scripts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = scripts[indexPath.row].title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let script = scripts[indexPath.row].content
+        
         let item = NSExtensionItem()
-        let argument: NSDictionary = ["customJavaScript": script.text]
+        let argument: NSDictionary = ["customJavaScript": script]
         let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
         let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
         item.attachments = [customJavaScript]
-        
+
         extensionContext?.completeRequest(returningItems: [item])
     }
+}
 
+
+struct Script{
+    let title: String
+    let content: String
 }
